296c647ffbaa0d4ff1f775bfdc5e18b8b73bb8c5
Zhannur <zhannurmyrzaqozy@gmail.com>
2025-11-26 22:41:19 +0500

    autodeploy final fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Quiz from "./quiz";
 import "./style.css";
+import { BrowserRouter } from "react-router-dom"; // если используешь роутер
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Quiz />
+    {/* Указываем basename для корректной работы в поддиректории */}
+    <BrowserRouter basename="/quizhistoryKZ/">
+      <Quiz />
+    </BrowserRouter>
   </React.StrictMode>
 );
